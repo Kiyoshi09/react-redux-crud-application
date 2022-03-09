18688656f1456fac788b3a8e6a4bb353efc64a63
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2022-03-09 22:22:55 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  const greeting = "puta";
-  const dom = <h1>{greeting}</h1>;
-  return dom;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
