e31f3227c9018ce51ba0f557ffb80ec5d609539b
Kiyoshi09 <8742202+Kiyoshi09@users.noreply.github.com>
2022-03-09 22:34:11 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from "react";
 
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10
+    },
+    {
+      name: "Heu",
+      age: 5
+    },
+    {
+      name: "NoName"
+    }
+  ];
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User key={index} name={profile.name} age={profile.age} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = (props) => {
+  return (
+    <div>
+      Hi, {props.name} and {props.age} だよ
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1
 };
 
 export default App;
